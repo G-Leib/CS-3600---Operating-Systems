385b141afe8437d4a48f17c3a0b2742bd67ea16f
gil <gil@debian>
2019-06-11 21:20:12 -0600

    final submission

--- Assignment_1/main.c
+++ Assignment_1/main.c
@@ -3,17 +3,20 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <assert.h>
+#include <stdlib.h>
 
 int main() {
     int fork_status = fork();
+    int fd1[2];
 
     if (fork_status > 0) {
         // Positive = Parent process
         assert(waitpid(-1, &fork_status, 0)!=0);
-        assert(printf("Child process exited with status: %d\n", fork_status)!=0);
+        assert(printf("Process exited with status: %d\n", fork_status)!=0);
     } else if (fork_status < 0) {
         // Negative = Error
-        perror("waitpid");
+        perror("fork error");
+        exit(-1);
     } else {
         // 0 = Child process
         int child_pid = getpid();
