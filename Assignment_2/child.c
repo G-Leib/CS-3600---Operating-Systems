57b47cd148dac339d3a8f60b843ede9e2b431a83
gil <gil@debian>
2019-06-19 20:53:07 -0600

    added last part of assignment

--- Assignment_2/child.c
+++ Assignment_2/child.c
@@ -4,11 +4,15 @@
 #include <unistd.h>
 #include <signal.h>
 
+
 int main() {
     pid_t parent = getppid();
 
     assert( kill( parent, SIGUSR1 ) == 0 );
     assert( kill( parent, SIGUSR2 ) == 0 );
     assert( kill( parent, SIGURG ) == 0 );
+    assert( kill( parent, SIGUSR2 ) == 0 );
+    assert( kill( parent, SIGUSR2 ) == 0 );
+    assert( kill( parent, SIGUSR2 ) == 0 );
     return(0);
 }
